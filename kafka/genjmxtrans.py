5b2780830c9c4d3bf0e31dea6729d3ec8244303b
Siddhartha Sahu <siddhartha.s@directi.com>
2015-12-24 15:02:25 +0530

    [kafka] Update template

--- kafka/genjmxtrans.py
+++ kafka/genjmxtrans.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import os
 import sys
-
 
 prod_zk_cluster = ["kafka01.myorg.com", "kafka02.myorg.com", "kafka03.myorg.com"]
 
@@ -12,8 +11,8 @@ zk_jmx_port="9011"
 
 graphite_host = "graphite.myorg.com"
 graphite_port = "2003"
+graphite_prefix = "kafka_cluster.servers"
 output_dir="output"
-
 
 def generate_zk(node_array):
     for host in node_array:
@@ -22,7 +21,7 @@ def generate_zk(node_array):
 def generate_host(host, type, template):
     print "generating template for %(type)s: %(host)s" % { 'host': host, 'type': type }
     output_file = open("%(output_dir)s/%(host)s-%(type)s.json" % { "output_dir": output_dir, "host": host, "type": type }, "w")
-    output_file.write(template % { "port" : zk_jmx_port , "host" : host, "graphite_host": graphite_host, "graphite_port": graphite_port  })
+    output_file.write(template % { "port" : zk_jmx_port , "host" : host, "graphite_host": graphite_host, "graphite_port": graphite_port, "graphite_prefix": graphite_prefix })
     output_file.close()
 
 generate_zk(prod_zk_cluster)
