7c6c6f74c033ef084066b71f6e1bf45c2153a860
Siddhartha Sahu <siddhartha.s@directi.com>
2015-12-31 16:04:13 +0530

    [tomcat] Update template and script

---
+++ tomcat/config.py
@@ -0,0 +1,11 @@
+servers = [
+    {"host":"172.16.0.1","port":"9111","server_alias":"tomcat-1"},
+    {"host":"172.16.0.2","port":"9011","server_alias":"tomcat-2"},
+    {"host":"172.16.0.3","port":"8880","server_alias":"tomcat-3"}
+]
+graphite_data = {
+    "graphite_host": "graphite.myorg.com",
+    "graphite_port": "2003",
+    "graphite_prefix": "zookeeperCluster.jmxmetrics"
+}
+output_dir="output"
